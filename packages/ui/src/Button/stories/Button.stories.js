0c41cff171b6126dd9e41213f57c16befa6b9d43
Sam Wight <samuelwight@gmail.com>
2019-02-08 16:54:14 -0600

    Added checkbox component

--- packages/ui/src/Button/stories/Button.stories.js
+++ packages/ui/src/Button/stories/Button.stories.js
@@ -1,9 +1,9 @@
-import React from "react"
-import createStory from "../../createStory"
+import React from 'react';
+import createStory from '../../createStory';
 
-import ButtonREADME from "./Button.README.md"
-import Button from "../Button"
+import ButtonREADME from './Button.README.md';
+import Button from '../Button';
 
-createStory("Button", "normal", ButtonREADME, () => (
-  <Button onClick={() => console.log("clicked")}>Click Me</Button>
-))
+createStory ('Button', 'normal', ButtonREADME, () => (
+  <Button onClick={() => console.log ('clicked')}> Click Me </Button>
+));
